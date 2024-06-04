692b5eede28df0ac63e01b7ac4740e53000ac1c9
Hydra <github@equipehydra.com>
2024-06-04 13:49:46 -0300

    update

---
+++ Scripts/titulo.js
@@ -0,0 +1,4 @@
+document.getElementById('i-rs').addEventListener('input', function() {
+            var rs = this.value;
+            document.title = "Termo de Responsabilidade [BANCO BS2] - " + rs;
+        });
